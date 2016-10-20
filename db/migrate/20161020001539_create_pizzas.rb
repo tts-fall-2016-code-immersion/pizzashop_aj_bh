be896c6779fca60283305fa3f5d6970e6fb0fb56
anesha <anesha.s.jones@gmail.com>
2016-10-20 00:06:52 -0400

    first commit

---
+++ db/migrate/20161020001539_create_pizzas.rb
@@ -0,0 +1,15 @@
+class CreatePizzas < ActiveRecord::Migration
+  def change
+    create_table :pizzas do |t|
+      t.string :name
+      t.string :crust
+      t.string :sauce
+      t.string :string
+      t.text :toppings
+      t.integer :cost
+      t.string :image
+
+      t.timestamps null: false
+    end
+  end
+end
